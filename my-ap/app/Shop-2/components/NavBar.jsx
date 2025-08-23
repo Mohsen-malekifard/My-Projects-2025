611b87b0c73ad43a85bd804be608da78aa5876b5
Mohsen Malekifard <mohsen.malekifard.dev@gmail.com>
2025-08-23 13:31:11 +0000

    F

--- my-ap/app/Shop-2/components/NavBar.jsx
+++ my-ap/app/Shop-2/components/NavBar.jsx
@@ -6,10 +6,11 @@ export default function NavBar() {
     const { cart } = useCart();
     const totalCount = cart.reduce((sum, item) => sum + item.qty, 0);
 
-    return(
+    return (
         <>
             <Link href = {"/Shop-2"} >Home</Link>
             <Link href = {"/Shop-2/cart"} >Cart ({totalCount})</Link>
         </>
     )
 }
+
