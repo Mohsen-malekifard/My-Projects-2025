bd53949f5a690214bee8e1ac7b9d539767b2058a
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-09-10 23:49:29 +0330

    Create page.jsx

---
+++ my-ap/app/Blog-2/page.jsx
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+
+export default function HtmlViewer() {
+  // مسیر فایل HTML داخل پوشه public
+  const filePath = path.join(process.cwd(), "public", "about.html");
+
+  // خوندن محتوای HTML
+  const htmlContent = fs.readFileSync(filePath, "utf8");
+
+  return (
+    <div className="p-5">
+      <h2 className="text-xl font-bold mb-4">📄 محتوای HTML:</h2>
+      <div
+        className="border p-4 rounded bg-white"
+        dangerouslySetInnerHTML={{ __html: htmlContent }}
+      />
+    </div>
+  );
+}
